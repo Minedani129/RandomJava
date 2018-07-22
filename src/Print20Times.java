51c22180170c06f93b4e4c0598eb2098648b336f
Minedani129 <40346785+Minedani129@users.noreply.github.com>
2018-07-21 17:16:03 -0700

    Make code commented and cleaner

--- src/Print20Times.java
+++ src/Print20Times.java
@@ -1,14 +1,28 @@
+/*
+ * This program gets a message from the user and prints it 20 times
+ * This program uses the Scanner class
+ * This program demonstrates the use of a for loop
+ * 
+ * The program receives input from the user, then, the if statement checks if it is a string.
+ * If yes, the input is stored in the variable MSG
+ * The for loop prints MSG twenty times.
+ *
+ */
+
 import java.util.Scanner;
 
 
 public class Print20Times
 {
 	static Scanner userInput = new Scanner(System.in);
+	
+	
 	public static void main(String[] args)
 	{
 		String msg = "N/A";
 		
 		System.out.println("Type in a message: ");
+		
 		if (userInput.hasNextLine())
 		{
 			msg = userInput.nextLine();
@@ -17,6 +31,7 @@ public class Print20Times
 		{
 			System.out.println("Type in words next time");
 		}
+		
 		
 		for (int i = 0; i < 20; i++)
 		{
